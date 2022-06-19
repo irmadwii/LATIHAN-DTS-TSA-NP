fc1628535229b4ccb7f3a6c16bdc6ee5d6456e6c
NetAcad DEVASC <changeme@example.com>
2022-06-19 13:39:46 +0000

    Program Python untuk manajemen Inventory

---
+++ inventory.py
@@ -0,0 +1,19 @@
+file = open("db-inventory.txt", "a")
+while True:
+    data = input("Input Data Inventory baru (ya/tidak)?")
+    print("***********************************")
+    if data == "ya" or data == "Ya":
+        x = input("Nama Perangkat: ")
+        y = input("Lokasi: ")
+        print("----------------------------------")
+    else:
+        file = open("db-inventory.txt", "r")
+        for item in file:
+            print(item)
+        file.close()
+        break
+
+    file.write("Nama Perangkat: " + (x + ", ") +"Lokasi: " + y + "\n")
+file.close()
+
+    
